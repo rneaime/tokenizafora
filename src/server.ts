de3a088c27569b49b92bebd087f771759493561e
rneaime <rneaime@gmail.com>
2025-05-25 20:48:58 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -85,3 +85,16 @@ app.post('/login', (req: Request, res: Response) => {
   const token = jwt.sign(usuario, 'chave_secreta', { expiresIn: '1h' });
   res.json({ autorizado: true, token, usuario });
 });
+
+app.post('/logout', (req: Request, res: Response) => {
+  // Lógica para desautenticar o usuário aqui
+  res.json({ autorizado: false });
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ mensagem: 'Rota não encontrada' });
+});
+
+app.listen(3001, () => {
+  console.log('Servidor está rodando na porta 3001');
+});
